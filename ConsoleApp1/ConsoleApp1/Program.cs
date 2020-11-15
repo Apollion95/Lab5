84cf350a5722473f21da1bc6859bdffe07cdfb2d
Apollion95 <ms051481@student.ath.edu.pl>
2020-11-15 11:14:26 +0100

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ////zadanie 1
+            int mm = 0;
+            int ss = 0;
+            int mmSum = 0;
+            int ssSum = 0;
+            int nrSesji = 0;
+            do
+            {
+                nrSesji++;
+                Console.WriteLine("Sesja " + nrSesji + "");
+                Console.WriteLine("Podaj ile minut sie uczyles?");
+                mm = System.Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj ile sekund sie uczyles?");
+                ss = System.Int32.Parse(Console.ReadLine());
+                mmSum += mm;
+                ssSum += ss;
+                Console.WriteLine("Czy wykonales kolejna sesje? y/n?");
+            } while ((Console.ReadLine() == "y") || (Console.ReadLine() == "y"));
+            Console.WriteLine("Lacznie uczyles sie " + mmSum + " minut " + ssSum + " sekund, i wykonales lacznie sesji " + nrSesji + "");
+
+            //Zadanie 2
+
+
+
+            int a = 0;
+            Console.WriteLine("Wprowadz ilosc pytan ");
+            a = int.Parse(Console.ReadLine());
+            string[] tablicaOdpowiedzi = new string[a];
+            for (int i = 0; i < tablicaOdpowiedzi.Length; i++)
+            {
+                Console.WriteLine("wpisz odpowiedz nr " + a + " A/B/C/D");
+                tablicaOdpowiedzi[i] = Console.ReadLine();
+
+            }
+            string[] tablicaOdp1 = new string[a]; //zakladamy ze odpowiedzi jest 10
+   
         }
-    }
+        }
 }
